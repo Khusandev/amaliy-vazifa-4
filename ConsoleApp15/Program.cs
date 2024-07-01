bff41ae1c492d3165b23aa50dff22935cf3f6413
Khusandev <husannabiev44@gmail.com>
2024-07-01 22:36:48 +0500

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            Console.Write("N1  sonini kiriting: (input):");
+
+            string N = Console.ReadLine();
+            int N1 = Convert.ToInt32(N);
+            Console.Write("Textni kiriting: (input):");
+            string str = Console.ReadLine();
+            string str2 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Length < N1)
+                {
+                    if (str[i] > 'a')
+                    { str2 += Convert.ToChar((str[i]) - ('a' - 'A')); }
+                    else str2 += str[i];
+                }
+                else if (str.Length > N1)
+                {
+                    if (str[i] < 'a')
+                    { str2 += Convert.ToChar((str[i]) + ('a' - 'A')); }
+                    else str2 += str[i];
+                }
+            }
+            Console.Write("Natija :");
+            for (int i = 0; i < str2.Length; i++)
+            {
+                Console.Write(str2[i]);
+            }
+        }
+    }
+}
